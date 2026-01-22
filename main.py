1e00d4f49d037e83956871db3bea43d6558a4edb
Manas <manasharma95@gmail.com>
2026-01-22 23:15:51 +0530

    Add task creation and listing API

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
+
+# Task Model (structure of task)
+class Task(BaseModel):
+    title: str
+    description: str
+
+# Temporary storage (in-memory database)
+tasks: List[Task] = []
 
 @app.get("/")
 def home():
     return {"message": "Task Manager API is running"}
+
+# Create a new task
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task)
+    return {"message": "Task added successfully", "task": task}
+
+# Get all tasks
+@app.get("/tasks")
+def get_tasks():
+    return tasks
